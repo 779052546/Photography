9bb4e085e2fb8ecea9e5cab5970b741274e3efb2
zbx <779052546@qq.com>
2017-07-09 14:37:11 +0800

    张丙辛 最新作品

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var conn = require('../mysql/db');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -47,55 +47,75 @@ router.get('/video',function(req,res){
 });
 
 router.get('/works',function(req,res){
-    res.render('works', {images:[
-        {id:1,name:'风吹起的裙角',src:'/images/works/1.jpg'},
-        {id:2,name:'起风了',src:'/images/works/2.jpg'},
-        {id:3,name:'时光恋人',src:'/images/works/3.jpg'},
-        {id:4,name:'午后蜜语',src:'/images/works/4.jpg'},
-        {id:5,name:'午后蜜语2',src:'/images/works/5.jpg'},
-        {id:6,name:'午后蜜语3',src:'/images/works/6.jpg'},
-        {id:7,name:'森之密藏Ⅱ',src:'/images/works/7.jpg'},
-        {id:8,name:'一见倾心',src:'/images/works/8.jpg'},
-        {id:9,name:'木质生活Ⅲ',src:'/images/works/9.jpg'}
-    ]});
+    conn.query('SELECT * FROM works',(err,results)=>{
+        if (err) {
+            next(err,req,res);
+            return;
+        }//返回错误页面
+        res.render('works', {images:results});
+    })
 });
 router.get('/enjoy',function(req,res){
-    res.render('enjoy', {images:[
-        {id:1,name:'客人嘉薇',src:'/images/enjoy/wedding-dresses/1.jpg'},
-        {id:2,name:'客人李倩',src:'/images/enjoy/wedding-dresses/2.jpg'},
-        {id:3,name:'客人梦醒',src:'/images/enjoy/wedding-dresses/3.jpg'},
-        {id:4,name:'客人瑜涵',src:'/images/enjoy/wedding-dresses/4.jpg'},
-        {id:5,name:'客人孟婕',src:'/images/enjoy/wedding-dresses/5.jpg'},
-        {id:6,name:'客人路阳',src:'/images/enjoy/wedding-dresses/6.jpg'},
-        {id:7,name:'客人津如',src:'/images/enjoy/wedding-dresses/7.jpg'},
-        {id:8,name:'客人海燕',src:'/images/enjoy/wedding-dresses/8.jpg'},
-        {id:9,name:'客人宋茜',src:'/images/enjoy/wedding-dresses/9.jpg'}
-    ]});
+    conn.query('SELECT * FROM enjoy',(err,results)=>{
+        if (err) {
+            next(err,req,res);
+            return;
+        }//返回错误页面
+        var photoAlbum = [
+            {id:1,name:'客人宁静',src:'../images/enjoy/photo-album/1.jpg'},
+            {id:2,name:'客人菲菲',src:'../images/enjoy/photo-album/2.jpg'},
+            {id:3,name:'客人莹',src:'../images/enjoy/photo-album/3.jpg'},
+            {id:4,name:'客人小美',src:'../images/enjoy/photo-album/4.jpg'},
+            {id:5,name:'客人小雨',src:'../images/enjoy/photo-album/5.jpg'},
+            {id:6,name:'客人莹',src:'../images/enjoy/photo-album/6.jpg'},
+            {id:7,name:'绽放',src:'../images/enjoy/photo-album/7.jpg'},
+            {id:8,name:'仲夏梦',src:'../images/enjoy/photo-album/8.jpg'},
+            {id:9,name:'森之迷藏',src:'../images/enjoy/photo-album/9.jpg'}
+        ];
+    var girlfriends = [
+        {id:1,name:'sisters',src:'../images/enjoy/girlfriends/1.jpg'},
+        {id:2,name:'李意蓉',src:'../images/enjoy/girlfriends/2.jpg'},
+        {id:3,name:'森之密藏',src:'../images/enjoy/girlfriends/3.jpg'},
+        {id:4,name:'客人田楠',src:'../images/enjoy/girlfriends/4.jpg'},
+        {id:5,name:'客人小雨',src:'../images/enjoy/girlfriends/5.jpg'},
+        {id:6,name:'青春与梦想',src:'../images/enjoy/girlfriends/6.jpg'},
+        {id:7,name:'sisters',src:'../images/enjoy/girlfriends/7.jpg'},
+        {id:8,name:'佳媛1',src:'../images/enjoy/girlfriends/8.jpg'},
+        {id:9,name:'佳媛2',src:'../images/enjoy/girlfriends/9.jpg'}
+    ];
+    var family = [
+        {id:1,name:'客人欣然',src:'../images/enjoy/family/1.jpg'},
+        {id:2,name:'客人梁坤',src:'../images/enjoy/family/2.jpg'},
+        {id:3,name:'客人 宝宝',src:'../images/enjoy/family/3.jpg'},
+        {id:4,name:'客人程红',src:'../images/enjoy/family/4.jpg'},
+        {id:5,name:'客人夏夏',src:'../images/enjoy/family/5.jpg'},
+        {id:6,name:'青春 真真',src:'../images/enjoy/family/6.jpg'},
+        {id:7,name:'客人琛',src:'../images/enjoy/family/7.jpg'},
+        {id:8,name:'客人君君',src:'../images/enjoy/family/8.jpg'},
+        {id:9,name:'客人颖颖',src:'../images/enjoy/family/9.jpg'}
+    ];
+        res.render('enjoy', {images:results,photoAlbum,girlfriends,family});
+    })
 });
 
 router.get('/theEarth',function(req,res){
-    var imagesIn = [
-        {id:1,name:'大理管霄',src:'../images/the-earth/1.jpg'},
-        {id:2,name:'大理婉玉',src:'../images/the-earth/2.jpg'},
-        {id:3,name:'大理婉玉',src:'../images/the-earth/3.jpg'},
-        {id:4,name:'大理婉玉',src:'../images/the-earth/4.jpg'},
-        {id:5,name:'大理管霄',src:'../images/the-earth/5.jpg'},
-        {id:6,name:'大理宇洁',src:'../images/the-earth/6.jpg'},
-        {id:7,name:'三亚艳霞',src:'../images/the-earth/7.jpg'},
-        {id:8,name:'三亚洁',src:'../images/the-earth/8.jpg'},
-        {id:9,name:'三亚夏',src:'../images/the-earth/9.jpg'}
-    ];
-    var imagesOut = [
-        {id:1,name:'巴厘岛 齐欢',src:'../images/the-earth/Out/1.jpg'},
-        {id:2,name:'巴厘岛 齐欢',src:'../images/the-earth/Out/2.jpg'},
-        {id:3,name:'巴厘岛 齐欢',src:'../images/the-earth/Out/3.jpg'},
-        {id:4,name:'巴厘岛齐欢',src:'../images/the-earth/Out/4.jpg'},
-        {id:5,name:'巴厘岛 彭乐',src:'../images/the-earth/Out/5.jpg'},
-        {id:6,name:'巴厘岛彭乐',src:'../images/the-earth/Out/6.jpg'},
-        {id:7,name:'巴厘岛彭乐',src:'../images/the-earth/Out/7.jpg'},
-        {id:8,name:'普吉街道篇',src:'../images/the-earth/Out/8.jpg'},
-        {id:9,name:'普吉姐妹篇',src:'../images/the-earth/Out/9.jpg'}
-    ]
-    res.render('theEarth', {imagesIn:imagesIn,imagesOut:imagesOut});
+    conn.query('SELECT * FROM earthin',(err,results)=>{
+        if (err) {
+            next(err,req,res);
+            return;
+        }//返回错误页面
+        var imagesOut = [
+            {id:1,name:'巴厘岛 齐欢',src:'../images/the-earth/Out/1.jpg'},
+            {id:2,name:'巴厘岛 齐欢',src:'../images/the-earth/Out/2.jpg'},
+            {id:3,name:'巴厘岛 齐欢',src:'../images/the-earth/Out/3.jpg'},
+            {id:4,name:'巴厘岛齐欢',src:'../images/the-earth/Out/4.jpg'},
+            {id:5,name:'巴厘岛 彭乐',src:'../images/the-earth/Out/5.jpg'},
+            {id:6,name:'巴厘岛彭乐',src:'../images/the-earth/Out/6.jpg'},
+            {id:7,name:'巴厘岛彭乐',src:'../images/the-earth/Out/7.jpg'},
+            {id:8,name:'普吉街道篇',src:'../images/the-earth/Out/8.jpg'},
+            {id:9,name:'普吉姐妹篇',src:'../images/the-earth/Out/9.jpg'}
+        ]
+        res.render('theEarth', {imagesIn:results,imagesOut});
+    })
 });
 module.exports = router;
